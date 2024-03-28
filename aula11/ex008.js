1e9f1230d4b96b98c93f6b0aacaa41919c9b579a
viniciusferraz963 <114557730+viniciusferraz963@users.noreply.github.com>
2024-03-28 11:09:14 -0300

    att

---
+++ aula11/ex008.js
@@ -0,0 +1,8 @@
+var vel = 78.2
+console.log (`A velocidade do seu carro é ${vel}Km/h`)
+if (vel > 60) { // condição simples!
+console.log(`Você ultrapassou a velocidade permitida. MULTADO!`)
+}
+console.log(`Dirija sempre usando o cinto de segurança!`)
+
+//console.log serve para escrever na tela no nodejs
---
+++ aula11/ex009.js
@@ -0,0 +1,9 @@
+var país = 'Brasil'
+console.log(`Vivendo no ${país}`)
+if (país == 'Brasil'){
+    console.log('Você é Brasileiro')
+} else {
+    console.log('Você é Estrangeiro')
+}
+
+// condição composta
